a8b7f53cd1000d1c4edb78d6a83503dffd6378ba
ChristopherNicholson <99701697+ChristopherNicholson@users.noreply.github.com>
2022-05-03 00:11:48 -0700

    Add files via upload

--- code/app/models.py
+++ code/app/models.py
@@ -1,6 +1,9 @@
-from app import db
-from app import login
+from re import I
+from app import db, login
 from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import InputRequired, Email, Length, DataRequired
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -11,6 +14,7 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     items = db.relationship('Item')
     cartitems = db.relationship('CartItem')
+    checkout = db.relationship('CheckoutInfo')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -19,30 +23,58 @@ class User(UserMixin, db.Model):
         return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
-        return f'''<User {self.username} sells {self.items}.
-                    Has in cart: {self.cartitems}>'''
+        return f'''<Seller: {self.username}, Item: {self.items},
+                    Cart: {self.cartitems}>'''
+
+class RegistrationForm(FlaskForm):
+    email = StringField('email', validators = [InputRequired()])
+    username = StringField('username', validators = [InputRequired()])
+    password = PasswordField('password', validators=[InputRequired()])
 
 class CartItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     itemname = db.Column(db.String(256))
-    cartholder = db.Column(db.Integer, db.ForeignKey('user.username'))
+    seller = db.Column(db.String(64), db.ForeignKey('user.username'))
     price = db.Column(db.Integer)
-    
+
     def __repr__(self):
-        return f'''<Cart Holder: {self.cartholder}, Item: {self.itemname}, 
-                    Price: {self.price}>'''
+        return f'''<Seller: {self.seller}, Item: {self.itemname}, 
+                    Price: ${self.price}>'''
+
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     itemname = db.Column(db.String(256))
     seller = db.Column(db.String(64), db.ForeignKey('user.username'))
     price = db.Column(db.Integer)
-    
+    rating = db.Column(db.Integer)
+    numberofratings = db.Column(db.Integer)
+    sumofratings = db.Column(db.Integer)
+
+    def updateRating(self, userrating):
+        self.numberofratings = self.numberofratings + 1
+        self.sumofratings = self.sumofratings + userrating
+        self.rating = self.sumofratings / self.numberofratings
+
     def __repr__(self):
         return f'''<Seller: {self.seller}, Item: {self.itemname},
-                    Price: {self.price}>'''
+                    Price: ${self.price}>, Rating: {self.rating}'''
+
+class CheckoutInfo(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    buyer = db.Column(db.String(64), db.ForeignKey('user.username'))
+    address = db.Column(db.String(64))
+    ccNumber = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f'''Buyer Name: {self.buyer}, Shipping address: {self.address}'''
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
 
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
-
