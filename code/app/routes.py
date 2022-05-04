debfeda86556332844015c3a8d7647d1a5611e98
Hieu Nguyen <hieutrinhnguyen1@gmail.com>
2022-05-03 19:56:37 -0700

    "added link to rate items on items page"

--- code/app/routes.py
+++ code/app/routes.py
@@ -28,7 +28,7 @@ def register():
         db.session.add(u)
         db.session.commit
         flash("Registration was successful")
-        return redirect(url_for('Login'))
+        return redirect(url_for('login'))
     return render_template('register.html')
 
 @login_required
@@ -45,7 +45,7 @@ def addItem():
             newItem = Item(seller=request.form["seller"], itemname=request.form["item"], price=request.form["price"], rating=0, numberofratings=0, sumofratings=0)
             db.session.add(newItem)
             db.session.commit()
-            return render_template('itemspage.html', items=Item.query.all())
+            return render_template('itemspage.html', items=Item.query.all()) 
     return render_template('itemspage.html', items=Item.query.all())
 
 
